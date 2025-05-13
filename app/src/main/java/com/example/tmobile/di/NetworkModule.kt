c475c6f708b4b9f1130e6c515d9e1954ae49725d
Nisha Jain <nishajain2797@gmail.com>
2025-05-13 19:02:21 +0530

    github

--- app/src/main/java/com/example/tmobile/di/NetworkModule.kt
+++ app/src/main/java/com/example/tmobile/di/NetworkModule.kt
@@ -15,6 +15,8 @@ object NetworkModule {
 
     @Provides
     fun provideBaseUrl() = "https://dummyjson.com/"
+    https://mocki.io/v1/6a683b81-2681-4915-a665-3bd24f5d8f85
+
 
     @Provides
     @Singleton
